05113ee87c1ab573cf00b862fd269551047180e1
Cody Bonney <me@codybonney.com>
2013-10-02 20:32:21 -0600

    Messing with models

--- js/sandbox.js
+++ js/sandbox.js
@@ -3,11 +3,11 @@ var Slider = (function () {
     function Slider(config) {
 	    var self = this;
 	    this.config = config;
-	    this.slides = new this.Slides;
-
-	    _.each(this.config.slides, function(slide, i) {
-		    self.slides.add(new self.Slide(slide));
-	    });
+	    this.slides = new this.Slides(
+		    _.map(self.config.slides, function(slide) {
+			    return new self.Slide(slide);
+		    })
+	    );
 
 		this.events = this._Events();
     }
