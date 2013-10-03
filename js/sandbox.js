e93ccb12bf93fda3b36c3bde1d78b262b0ac47be
Cody Bonney <me@codybonney.com>
2013-10-02 20:52:01 -0600

    Messing with models

--- js/sandbox.js
+++ js/sandbox.js
@@ -2,31 +2,38 @@ var Slider = (function () {
 
     function Slider(config) {
 	    var self = this;
+		self.Models = self._Models();
+	    self.Collections = self._Collections();
+
 	    this.config = config;
-	    this.slides = new this.Slides(
+	    this.slides = new self.Collections.slides(
 		    _.map(self.config.slides, function(slide) {
-			    return new self.Slide(slide);
+			    return new self.Models.slide(slide);
 		    })
 	    );
-
 		this.events = this._Events();
     }
 
-	Slider.prototype.Slide = Backbone.Model.extend({
-		promptColor: function() {
-			var cssColor = prompt("Please enter a CSS color:");
-			this.set({
-				color: cssColor
-			});
-		}
-	});
+	Slider.prototype._Models = function() {
+		var self = this;
+		return {
+			slide : Backbone.Model.extend({
 
-	Slider.prototype.Slides = Backbone.Collection.extend({
-		model : this.Slide
-	});
+			})
+		}
+	};
+
+	Slider.prototype._Collections = function() {
+		var self = this;
+		return {
+			slides :  Backbone.Collection.extend({
+				model : self.Models.slide
+			})
+		}
+	};
 
 	Slider.prototype._Events = function () {
-		this.slides.on('change:color', function(model, color) {
+		this.slides.on('change:background', function(model, color) {
 			$('body').css({background: color});
 		});
 	};
